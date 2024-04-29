acc91029ae77a21d554d211c4b59a74fcff18c28
mariane vieira golnçalves <mariane.goncalves@portalsesisp.org.br>
2024-04-29 14:54:10 -0300

    criar o navigation configurar o navigation

--- app/src/main/java/com/happyhour/safeguardpro/view/InicialFuncionarioFragment.kt
+++ app/src/main/java/com/happyhour/safeguardpro/view/InicialFuncionarioFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [IncialFuncionarioFragment.newInstance] factory method to
+ * Use the [InicialFuncionarioFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class IncialFuncionarioFragment : Fragment() {
+class InicialFuncionarioFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class IncialFuncionarioFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_incial_funcionario, container, false)
+        return inflater.inflate(R.layout.fragment_inicial_funcionario, container, false)
     }
 
     companion object {
@@ -50,7 +50,7 @@ class IncialFuncionarioFragment : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            IncialFuncionarioFragment().apply {
+            InicialFuncionarioFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
--- app/src/main/java/com/happyhour/safeguardpro/view/MainActivity.kt
+++ app/src/main/java/com/happyhour/safeguardpro/view/MainActivity.kt
@@ -2,13 +2,34 @@ package com.happyhour.safeguardpro.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.happyhour.safeguardpro.R
+import com.happyhour.safeguardpro.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    //Criar a toolbar
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    //Criar a navigation
+    private lateinit var  navController: NavController
+
+    //Criar o binding
+    private var _binding: ActivityMainBinding? = null
+    private val binding : ActivityMainBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
 
-
+        //Configurar a navegação e a toolbar
+        val navHostFragment = supportFragmentManager.findFragmentById(binding.fragmentContainerView.id) as NavHostFragment
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
